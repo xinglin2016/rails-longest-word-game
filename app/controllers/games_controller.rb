054795c91e872d19034542bea8d2d4cf79d3694a
xinglin2016 <lixinglin0829@gmail.com>
2021-02-17 21:13:33 -0500

    add score method in controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,5 +9,21 @@ class GamesController < ApplicationController
 
   def score
     # raise
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @valid_word = valid_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def valid_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
